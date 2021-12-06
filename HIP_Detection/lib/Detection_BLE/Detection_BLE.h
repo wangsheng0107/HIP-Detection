f4a3f1381aa327459172f23b5a71a2a0b6305654
wangsheng0107 <75300390+wangsheng0107@users.noreply.github.com>
2021-12-06 02:50:04 -0500

    Add files via upload

---
+++ HIP_Detection/lib/Detection_BLE/Detection_BLE.h
@@ -0,0 +1,41 @@
+/***************************************************
+  This is a library for the HIP Detection
+
+  Use BLE to send & receive data and command from/to a ble master device
+  Control DAC80501 and receive data from ADS1235
+
+  Sheng Wang Dec 2021
+ ****************************************************/
+
+#ifndef Detection_BLE_H
+#define Detection_BLE_H
+
+
+#include <ArduinoBLE.h>
+#include "ADS1235.h"
+#include "DAC80501.h"
+
+
+
+class DetectionBLE  {
+public: 
+  DetectionBLE(ADS1235 & ADC, DAC80501 & DAC): _adc(ADC), _dac(DAC){};
+  ~DetectionBLE(){};
+public:
+int begin();
+int read_pct();
+// int update();
+ 
+int write_dac(int ble_dac_tgt);
+
+
+private:
+ ADS1235 & _adc;
+ DAC80501 & _dac;
+ int _dac_pct;
+
+};
+
+#endif
+
+
