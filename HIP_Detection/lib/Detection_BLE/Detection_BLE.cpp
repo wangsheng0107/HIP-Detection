9e2c375b5a7af1bf7133525ebc17086db5d1d330
wangsheng0107 <75300390+wangsheng0107@users.noreply.github.com>
2021-12-07 01:09:26 -0500

    Add files via upload

--- HIP_Detection/lib/Detection_BLE/Detection_BLE.cpp
+++ HIP_Detection/lib/Detection_BLE/Detection_BLE.cpp
@@ -1,42 +1,74 @@
-// #include "Detection_BLE.h"
+#include "Detection_BLE.h"
 
 
 
 
-//  int DetectionBLE::begin(){
 
-//     if(!BLE.begin()){ 
-//       Serial.print("Starting BLE failed");
-//       while (1);
-//     }
+int DetectionBLE::begin(){
+
+  if(!BLE.begin()){ 
+    Serial.print("Starting BLE failed");
+    while (1);
+  }
 
 
-//     BLE.setLocalName("HIP-Detection");
-//     // set the UUID for the service this peripheral advertises:
-//     BLE.setAdvertisedService(DetectionServcie);
+  BLE.setLocalName("HIP-Detection");
+  // set the UUID for the service this peripheral advertises:
+  BLE.setAdvertisedService(DetectionServcie);
 
-//     // add the characteristics to the service
-//     DetectionServcie.addCharacteristic(switchCharacteristic);
-//     DetectionServcie.addCharacteristic(LaserCharacteristic);
-//     DetectionServcie.addCharacteristic(RDATACharacterstic);
-//     // assign event handlers for connected, disconnected to peripheral
-//     BLE.setEventHandler(BLEConnected, blePeripheralConnectHandler);
-//     BLE.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
-//     // add the service
-//     BLE.addService(DetectionServcie);
+  // add the characteristics to the service
+  DetectionServcie.addCharacteristic(switchCharacteristic);
+  DetectionServcie.addCharacteristic(LaserCharacteristic);
+  DetectionServcie.addCharacteristic(RDATACharacterstic);
 
-//     // set initial values for switch and laser control
-//     switchCharacteristic.writeValue(0);
-//     LaserCharacteristic.writeValue(0);
+  // add the service
+  BLE.addService(DetectionServcie);
 
-//     // start advertising
-//     if(!BLE.advertise()){
-//       Serial.println("Bluetooth Advertise failed.");
-//       return 0;
-//     }
-//     else{
-//     Serial.println("Bluetooth device advertising, waiting for connections...");
+  // assign event handlers for connected, disconnected to peripheral
+  BLE.setEventHandler(BLEConnected, blePeripheralConnectHandler);
+  BLE.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
 
-//     return 1;
-//     }
-//   }
+  // assign event handlers for characteristic
+  switchCharacteristic.setEventHandler(BLEWritten, switchCharacteristicWritten);
+
+  // set initial values for switch and laser control
+  switchCharacteristic.writeValue(0);
+  LaserCharacteristic.writeValue(0);
+
+  // start advertising
+  if(!BLE.advertise()){
+    Serial.println("Bluetooth Advertise failed.");
+    return 0;
+  }
+  else{
+  Serial.println("Bluetooth device advertising, waiting for connections...");
+
+  return 1;
+  }
+}
+
+
+void blePeripheralConnectHandler(BLEDevice central) {
+  // central connected event handler
+  Serial.print("Connected event, central: ");
+  Serial.println(central.address());
+}
+
+void blePeripheralDisconnectHandler(BLEDevice central) {
+  // central disconnected event handler
+  Serial.print("Disconnected event, central: ");
+  Serial.println(central.address());
+}
+
+void switchCharacteristicWritten(BLEDevice central, BLECharacteristic characteristic) {
+  // central wrote new value to characteristic, update LED
+  Serial.print("Characteristic event, written: ");
+
+  if (switchCharacteristic.value()) {
+    Serial.println("LED on");
+    digitalWrite(13, HIGH);
+  } else {
+    Serial.println("LED off");
+    digitalWrite(13, LOW);
+  }
+}
